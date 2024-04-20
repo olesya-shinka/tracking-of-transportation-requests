020caaaa283d2fa7edc3102b4b6d2918c9b3b3b6
Olesya <os1124856@gmail.com>
2024-04-20 14:28:21 +0500

    add components

---
+++ src/components/main/index.tsx
@@ -0,0 +1,245 @@
+import { useEffect, useState } from "react";
+import "../../App.css";
+import { Order } from "../../App";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import ModalEdit from "../modal";
+import { FiEdit } from "react-icons/fi";
+import { AiOutlineDelete } from "react-icons/ai";
+import { TfiComments } from "react-icons/tfi";
+import {
+  deleteOrder,
+  editOrder,
+  saveOrdersToLocalStorage,
+} from "../../store/slice/orderSlice";
+import ModalAdd from "../modal/indexNew";
+
+function Main() {
+  const dispatch = useDispatch();
+  const orders = useSelector((state: RootState) => state.orders);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [statusFilter, setStatusFilter] = useState<string | null>(null);
+  const [isAdminMode, setIsAdminMode] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [hideCompleted, setHideCompleted] = useState<boolean>(false);
+  const [filteredOrders, setFilteredOrders] = useState<Order[]>(orders);
+  const [filteredAndHiddenOrders, setFilteredAndHiddenOrders] =
+    useState<Order[]>(orders);
+  useEffect(() => {
+    const storedOrders = localStorage.getItem("ordersState");
+    if (storedOrders) {
+      dispatch(saveOrdersToLocalStorage(JSON.parse(storedOrders)));
+    }
+    dispatch(saveOrdersToLocalStorage());
+    setFilteredAndHiddenOrders(orders);
+  }, [dispatch, orders]);
+
+  useEffect(() => {
+    let filtered = orders;
+    if (statusFilter) {
+      filtered = filtered.filter((order) => order.status === statusFilter);
+    }
+    if (hideCompleted) {
+      filtered = filtered.filter((order) => order.status !== "завершено");
+    }
+    setFilteredOrders(filtered);
+  }, [orders, statusFilter, hideCompleted]);
+
+  useEffect(() => {
+    let filtered = filteredOrders;
+    if (hideCompleted) {
+      filtered = filtered.filter((order) => order.status !== "завершено");
+    }
+    setFilteredAndHiddenOrders(filtered);
+  }, [filteredOrders, hideCompleted]);
+  const handleHideCompletedChange = () => {
+    setHideCompleted(!hideCompleted);
+    setFilteredAndHiddenOrders(
+      !hideCompleted
+        ? filteredOrders.filter((order) => order.status !== "завершено")
+        : filteredOrders
+    );
+  };
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+    const filtered = orders.filter((order) =>
+      Object.values(order).some(
+        (value) =>
+          typeof value === "string" &&
+          value.toLowerCase().includes(term.toLowerCase())
+      )
+    );
+    setFilteredOrders(filtered);
+  };
+
+  const handleStatusFilter = (status: string | null) => {
+    setStatusFilter(status);
+    const filtered = status
+      ? orders.filter((order) => order.status === status)
+      : orders;
+    setFilteredOrders(filtered);
+    setFilteredAndHiddenOrders(
+      hideCompleted
+        ? filtered.filter((order) => order.status !== "завершено")
+        : filtered
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    dispatch(deleteOrder(id));
+
+    const updatedFilteredOrders = filteredOrders.filter(
+      (order) => order.id !== id
+    );
+    setFilteredOrders(updatedFilteredOrders);
+
+    const updatedFilteredAndHiddenOrders = filteredAndHiddenOrders.filter(
+      (order) => order.id !== id
+    );
+    setFilteredAndHiddenOrders(updatedFilteredAndHiddenOrders);
+  };
+
+  const handleEdit = (editedOrder: Order) => {
+    dispatch(editOrder(editedOrder));
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  const handleOpenModalEdit = (order: Order) => {
+    setSelectedOrder(order);
+    setOpenModalEdit(true);
+  };
+
+  const handleCloseModalEdit = () => {
+    setOpenModalEdit(false);
+  };
+
+  return (
+    <div className="App">
+      <div className="switch-box">
+        <p>АДМИНИСТРАТОР</p>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isAdminMode}
+            onChange={(e) => setIsAdminMode(e.target.checked)}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <div className="content">
+        <h1>
+          ЗАКАЗЫ <span>{filteredOrders.length}</span>
+        </h1>
+        <div className="content-box">
+          <input
+            type="text"
+            placeholder="Поиск по клиентам"
+            value={searchTerm}
+            onChange={(e) => handleSearch(e.target.value)}
+            className="content-box-input"
+          />
+          <select
+            value={statusFilter || ""}
+            onChange={(e) => handleStatusFilter(e.target.value || null)}
+            className="content-box-select"
+          >
+            <option value="">Все</option>
+            <option value="в работе">В работе</option>
+            <option value="завершено">Завершено</option>
+          </select>
+          <label className="label-completed">
+            <input
+              type="checkbox"
+              checked={hideCompleted}
+              onChange={handleHideCompletedChange}
+            />
+            Скрыть завершенные заказы
+          </label>
+          {isAdminMode && (
+            <button className="content-box-btn" onClick={handleOpenModal}>
+              Создать заказ
+            </button>
+          )}
+          {openModal && (
+            <ModalAdd
+              handleCloseModal={handleCloseModal}
+              handleOpenModal={handleOpenModal}
+              filteredOrders={filteredOrders}
+              hideCompleted={hideCompleted}
+              setFilteredOrders={setFilteredOrders}
+              setFilteredAndHiddenOrders={setFilteredAndHiddenOrders}
+            />
+          )}
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>ДАТА</th>
+              <th>КЛИЕНТ</th>
+              <th>ПЕРЕВОЗЧИК</th>
+              <th>ТЕЛЕФОН</th>
+              <th>СТАТУС</th>
+              <th>ATI Code</th>
+              {isAdminMode && <th></th>}
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAndHiddenOrders.map((order, id) => (
+              <tr key={id}>
+                <td>{order.id}</td>
+                <td>{order.date}</td>
+                <td>{order.client}</td>
+                <td>{order.carrier}</td>
+                <td>{order.phone}</td>
+                <td>{order.status}</td>
+                <td>
+                  <a
+                    href={`https://ati.su/firms/${order.atiCode}/info`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {order.atiCode}
+                  </a>
+                </td>
+                {isAdminMode && (
+                  <td className="td-actions">
+                    <FiEdit onClick={() => handleOpenModalEdit(order)} />
+                    {openModalEdit && selectedOrder && (
+                      <ModalEdit
+                        handleCloseModalEdit={handleCloseModalEdit}
+                        handleEdit={handleEdit}
+                        order={selectedOrder}
+                      />
+                    )}
+                    <AiOutlineDelete onClick={() => handleDelete(order.id)} />
+                    <div className="tooltip-container">
+                      {order.comments.length === 0 ? (
+                        <span className="tooltip-text">Нет комментария</span>
+                      ) : (
+                        <span className="tooltip-text">{order.comments}</span>
+                      )}
+                      <TfiComments />
+                    </div>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/components/modal/indexNew.tsx
@@ -0,0 +1,158 @@
+import { useState } from "react";
+import { Order } from "../../App";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { useDispatch } from "react-redux";
+import { addOrder } from "../../store/slice/orderSlice";
+
+interface ModalAddProps {
+  handleCloseModal: () => void;
+  handleOpenModal: () => void;
+  filteredOrders: Order[];
+  hideCompleted: boolean;
+  setFilteredOrders: React.Dispatch<React.SetStateAction<Order[]>>;
+  setFilteredAndHiddenOrders: React.Dispatch<React.SetStateAction<Order[]>>;
+}
+
+function ModalAdd({
+  handleCloseModal,
+  handleOpenModal,
+  filteredOrders,
+  hideCompleted,
+  setFilteredOrders,
+  setFilteredAndHiddenOrders,
+}: ModalAddProps) {
+  const dispatch = useDispatch();
+  const [date, setDate] = useState<string>("");
+  const [client, setClient] = useState<string>("");
+  const [carrier, setCarrier] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [comment, setComment] = useState<string>("");
+  const [status, setStatus] = useState<string>("новый");
+  const [atiCode, setAtiCode] = useState<string>("");
+  const orders = useSelector((state: RootState) => state.orders);
+
+  const handleCreate = () => {
+    const newId = orders.length + 1;
+    const orderToAdd: Order = {
+      id: newId,
+      date,
+      client,
+      carrier,
+      phone,
+      comments: comment,
+      status,
+      atiCode,
+    };
+    dispatch(addOrder(orderToAdd));
+    handleOpenModal();
+
+    const updatedFilteredOrders = [...filteredOrders, orderToAdd].filter(
+      (order) => {
+        if (hideCompleted) {
+          return order.status !== "завершено";
+        }
+        return true;
+      }
+    );
+    setFilteredOrders(updatedFilteredOrders);
+
+    const updatedFilteredAndHiddenOrders = [...updatedFilteredOrders];
+    setFilteredAndHiddenOrders(updatedFilteredAndHiddenOrders);
+
+    setDate("");
+    setCarrier("");
+    setClient("");
+    setPhone("");
+    setComment("");
+    setAtiCode("");
+    handleCloseModal();
+  };
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <h2>Создание нового заказа</h2>
+        <label className="modal-label">
+          Дата:
+          <input
+            className="modal-input"
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          Клиент:
+          <input
+            className="modal-input"
+            type="text"
+            name="client"
+            value={client}
+            onChange={(e) => setClient(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          Перевозчик:
+          <input
+            className="modal-input"
+            type="text"
+            name="carrier"
+            value={carrier}
+            onChange={(e) => setCarrier(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          Телефон:
+          <input
+            className="modal-input"
+            type="phone"
+            name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          Статус:
+          <input
+            className="modal-input"
+            type="text"
+            name="status"
+            defaultValue={status}
+            onChange={(e) => setStatus(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          ATI Code:
+          <input
+            className="modal-input"
+            type="text"
+            name="atiCode"
+            value={atiCode}
+            onChange={(e) => setAtiCode(e.target.value)}
+          />
+        </label>
+        <label className="modal-label">
+          Комментарий:
+          <input
+            className="modal-input"
+            type="text"
+            name="comments"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </label>
+        <div className="modal-box-btn">
+          <button className="modal-button" onClick={handleCreate}>
+            Создать заказ
+          </button>
+          <button className="modal-button" onClick={handleCloseModal}>
+            Отмена
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalAdd;
